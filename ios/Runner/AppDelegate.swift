f3b8173581b47662e4d537f98e7edc657c9c1a9d
dvaldes1238 <david.valdes@getfaircare.de>
2023-10-26 15:36:01 +0200

    added gmaps api key for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyByieLr5w0TCgiP337aYYg2i7UgNH01Lec")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
