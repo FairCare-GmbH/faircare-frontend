86631c108036519c3a907c558927c484de4f1f1a
MubeenNaeem <mubeen.naeem138@gmail.com>
2023-10-28 18:24:47 +0500

    uncommented prefs page

--- lib/views/vacation_details/calendar_widget.dart
+++ lib/views/vacation_details/calendar_widget.dart
@@ -1,13 +1,4 @@
-import 'dart:math';
-
-import 'package:faircare/global/colors.dart';
-import 'package:faircare/global/text_style.dart';
-import 'package:faircare/widgets/calendar_day_item.dart';
-import 'package:faircare/widgets/spacer.dart';
-import 'package:faircare/widgets/triangles.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 class VacationDetailsCalendarWidget extends StatefulWidget {
   const VacationDetailsCalendarWidget({Key? key}) : super(key: key);
