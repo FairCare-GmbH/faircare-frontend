7091900355141e7b35db9961755e06cbaac37ac0
dvaldes1238 <david.valdes@getfaircare.de>
2023-11-12 20:17:13 +0100

    locked to portait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:faircare/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const FaircareApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Than we setup preferred orientations,
+  // and only after it finished we run our app
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(const FaircareApp()));
 }
